6b7dfdf2351f99da3061f17eb7f13b7249b4120e
白雪鹏 <baixp@dustess.com>
2021-10-16 16:34:13 +0800

    feat: 优化代码

--- src/TodoList/template.ts
+++ src/TodoList/template.ts
@@ -3,7 +3,7 @@ import { ITodo } from "./index";
 export function todoView({ id, content, completed }: ITodo ): string {
     return `
         <input type="checkbox" ${ completed ? 'checked' : '' } data-id="${ id }" />
-        <span style="text-decoration: ${ completed ? 'line-through' : '' }">${ content }</span>
+        <span style="text-decoration: ${ completed ? 'line-through' : 'none' }">${ content }</span>
         <button data-id="${ id }">删除</button>
       `
 }
